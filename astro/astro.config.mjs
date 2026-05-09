df7c1082b72cf1ceedde5539cc761593d46c4491
brian-montgomery <brian-montgomery@users.noreply.github.com>
2026-05-09 17:04:38 -0400

    Version 0.5.1 (#15)
    * Astro and Starlight update
    * Wording updates and clarifications

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -38,8 +38,9 @@ export default defineConfig({
 			sidebar: [
         {
           label: `Rules v${version}`,
-          badge: 'New',
-        	autogenerate: { directory: 'rules', collapsed: true, },
+          items: [{
+            autogenerate: { directory: 'rules', }},
+          ],
           collapsed: true,
         },
         {
@@ -90,7 +91,9 @@ export default defineConfig({
         {
           label: 'Design Notes',
           collapsed: true,
-          autogenerate: { directory: 'design-notes', collapsed: true, },
+          items: [{
+            autogenerate: { directory: 'design-notes' },
+          }]
         },
 			],
       plugins: [
